5ce8cade5927201b815f90f12ebf52ebc23a384f
AJ <aletha.e.jones@gmail.com>
2024-01-15 11:53:35 +0000

    Floating time

--- Platforms/Android/AndroidWS.cs
+++ Platforms/Android/AndroidWS.cs
@@ -6,208 +6,207 @@ using Android.Net.Wifi;
 using Android.OS;
 using SDD = System.Diagnostics.Debug;
 
-namespace WifiScannerLib
+namespace WifiScannerLib;
+
+// All the code in this file is only included on Android.
+[Service(Exported = true)]
+public class AndroidWS : Service, IWS
 {
-    // All the code in this file is only included on Android.
-    [Service(Exported = true)]
-    public class AndroidWS : Service, IWS
+    private WifiManager? WM;
+    private SRC NetWB { get; set; }
+
+    private bool IsRegistered = false, Run = false;
+
+    private List<WifiInfoItem> ScanData = new List<WifiInfoItem>();
+
+    public Context? CTX = null;
+
+    public event EventHandler ScanReturned;
+
+    /// <summary>
+    /// Parametrised constructor to assign context to the scanner object
+    /// </summary>
+    /// <param name="_CTX">Context</param>
+    public AndroidWS(Context _CTX)
     {
-        private WifiManager? WM;
-        private SRC NetWB { get; set; }
+        CTX = _CTX;
 
-        private bool IsRegistered = false, Run = false;
+        //SDD.WriteLine("**** Constructor called ****");
 
-        private List<WifiInfoItem> ScanData = new List<WifiInfoItem>();
+        Initialise();
+    }
 
-        public Context? CTX = null;
+    public AndroidWS()
+    { }
 
-        public event EventHandler ScanReturned;
+    /// <summary>
+    /// Needed by Service inheritance. Think it's called when service is created
+    /// </summary>
+    public override void OnCreate()
+    { Initialise(); }
 
-        /// <summary>
-        /// Parametrised constructor to assign context to the scanner object
-        /// </summary>
-        /// <param name="_CTX">Context</param>
-        public AndroidWS(Context _CTX)
-        {
-            CTX = _CTX;
+    /// <summary>
+    /// Initialises the scanner
+    /// </summary>
+    /// <exception cref="NullReferenceException">Thrown when context is missing</exception>
+    /// <exception cref="Exception">Thrown when WM (WifiManager) is null</exception>
+    private void Initialise()
+    {
+        //SDD.WriteLine("**** Initialise called ****");
 
-            //SDD.WriteLine("**** Constructor called ****");
-
-            Initialise();
-        }
-
-        public AndroidWS()
-        { }
-
-        /// <summary>
-        /// Needed by Service inheritance. Think it's called when service is created
-        /// </summary>
-        public override void OnCreate()
-        { Initialise(); }
-
-        /// <summary>
-        /// Initialises the scanner
-        /// </summary>
-        /// <exception cref="NullReferenceException">Thrown when context is missing</exception>
-        /// <exception cref="Exception">Thrown when WM (WifiManager) is null</exception>
-        private void Initialise()
-        {
-            //SDD.WriteLine("**** Initialise called ****");
-
-            if (CTX == null)
-            { throw new NullReferenceException("CTX was null?!"); }
+        if (CTX == null)
+        { throw new NullReferenceException("CTX was null?!"); }
 
 #if ANDROID23_0_OR_GREATER
-            if (
-                CTX.CheckSelfPermission(Android.Manifest.Permission.AccessWifiState) == Permission.Denied ||
-                CTX.CheckSelfPermission(Android.Manifest.Permission.AccessFineLocation) == Permission.Denied ||
-                CTX.CheckSelfPermission(Android.Manifest.Permission.ChangeWifiState) == Permission.Denied
-                )
-            {
-                throw new Exception("Perms issue!");
-            }
+        if (
+            CTX.CheckSelfPermission(Android.Manifest.Permission.AccessWifiState) == Permission.Denied ||
+            CTX.CheckSelfPermission(Android.Manifest.Permission.AccessFineLocation) == Permission.Denied ||
+            CTX.CheckSelfPermission(Android.Manifest.Permission.ChangeWifiState) == Permission.Denied
+            )
+        {
+            throw new Exception("Perms issue!");
+        }
 #endif
 
-            WM = (WifiManager)CTX.GetSystemService(WifiService);
+        WM = (WifiManager)CTX.GetSystemService(WifiService);
 
-            if (WM == null || WM.IsScanThrottleEnabled)
-            { throw new Exception("WM issue!"); }
+        if (WM == null || WM.IsScanThrottleEnabled)
+        { throw new Exception("WM issue!"); }
 
-            NetWB = new SRC();
+        NetWB = new SRC();
 
-            NetWB.Initialise(CB);
-        }
-
-        public void StopScanner()
-        { Run = false; }
-
-        /// <summary>
-        /// Triggers a scan. Subscribe to <c>ScanReturned</c> for data
-        /// </summary>
-        public void CollectData()
-        {
-            SDD.WriteLine("Android called!");
-
-            Task.Run(() =>
-            { WM.StartScan(); });
-            //wtf android docs, this function is supposed to be
-            //depreciated, and is left with no clear alternative
-            //YET, it is the only mechanism that works to actually
-            //trigger a scan with up to date info? istg
-
-
-            if (!IsRegistered)
-            {
-                WM.RegisterScanResultsCallback
-                (
-                    CTX.MainExecutor,
-                    NetWB
-                );
-
-                IsRegistered = true;
-            }
-        }
-
-        /// <summary>
-        /// Callback for Connectivity Manager
-        /// </summary>
-        private void CB()
-        {
-            SDD.WriteLine("Call back!");
-
-            ScanData.Clear();
-
-            if (WM.ScanResults.Count > 0)
-            {
-                foreach (var N in WM.ScanResults)
-                { ScanData.Add(new WifiInfoItem(N)); }
-
-                ScanData = ScanData
-                    .OrderByDescending(X => X._RSSI)
-                    .ToList();
-
-                OnScanResult(new WifiEvent(ScanData));
-            }
-            else
-            { OnScanResult(new WifiEvent()); }
-        }
-
-        /// <summary>
-        /// Wrapper for ScanReturn event
-        /// </summary>
-        /// <param name="We">WifiEvent args</param>
-        private void OnScanResult(WifiEvent We)
-        { ScanReturned?.Invoke(this, We); }
-
-        /// <summary>
-        /// Mandated by Service inheritance
-        /// </summary>
-        public override IBinder? OnBind(Intent? _Int)
-        {
-#if ANDROID29_0_OR_GREATER
-            return new Binder(_Int.Action);
-#endif
-            throw new Exception("idfk what this function's supposed to do");
-        }
-
-        public Device DType()
-        { return Device.Android; }
-
-        public bool CheckLocation()
-        {
-            using (LocationManager LM = (LocationManager)CTX.GetSystemService(LocationService))
-            {
-#if ANDROID28_0_OR_GREATER
-                return LM.IsLocationEnabled;
-#endif
-            }
-        }
-
-        #region Old
-        //is called to collect data
-        /*public IEnumerable<WifiInfoItem> GetData()
-        {
-            //SDD.WriteLine("**** Get Data called ****");
-
-            if (CM == null)
-            { throw new Exception("CM was null!"); }
-
-            if (IsRegistered)
-            {
-                CM.UnregisterNetworkCallback(NetCB);
-                IsRegistered = false;
-            }
-
-            if (!IsRegistered)
-            {
-                CM.RegisterNetworkCallback
-                (
-                    new NetworkRequest.Builder().AddTransportType(TransportType.Wifi).Build(),
-                    NetCB
-                );
-                IsRegistered = true;
-            }
-
-            //SDD.WriteLine($"***** CollectData results {ScanData.Count} *****");
-
-            return ScanData;
-        } */
-
-
-        #endregion
+        NetWB.Initialise(CB);
     }
 
-    public class SRC : WifiManager.ScanResultsCallback
+    public void StopScanner()
+    { Run = false; }
+
+    /// <summary>
+    /// Triggers a scan. Subscribe to <c>ScanReturned</c> for data
+    /// </summary>
+    public void CollectData()
     {
-        public Action CallBack { get; set; }
+        SDD.WriteLine("Android called!");
 
-        public void Initialise(Action _CallBack)
-        { CallBack = _CallBack; }
+        Task.Run(() =>
+        { WM.StartScan(); });
+        //wtf android docs, this function is supposed to be
+        //depreciated, and is left with no clear alternative
+        //YET, it is the only mechanism that works to actually
+        //trigger a scan with up to date info? istg
 
-        public SRC()
-        { }
 
-        public override void OnScanResultsAvailable()
-        { CallBack(); }
+        if (!IsRegistered)
+        {
+            WM.RegisterScanResultsCallback
+            (
+                CTX.MainExecutor,
+                NetWB
+            );
+
+            IsRegistered = true;
+        }
     }
+
+    /// <summary>
+    /// Callback for Connectivity Manager
+    /// </summary>
+    private void CB()
+    {
+        SDD.WriteLine("Call back!");
+
+        ScanData.Clear();
+
+        if (WM.ScanResults.Count > 0)
+        {
+            foreach (var N in WM.ScanResults)
+            { ScanData.Add(new WifiInfoItem(N)); }
+
+            ScanData = ScanData
+                .OrderByDescending(X => X._RSSI)
+                .ToList();
+
+            OnScanResult(new WifiEvent(ScanData));
+        }
+        else
+        { OnScanResult(new WifiEvent()); }
+    }
+
+    /// <summary>
+    /// Wrapper for ScanReturn event
+    /// </summary>
+    /// <param name="We">WifiEvent args</param>
+    private void OnScanResult(WifiEvent We)
+    { ScanReturned?.Invoke(this, We); }
+
+    /// <summary>
+    /// Mandated by Service inheritance
+    /// </summary>
+    public override IBinder? OnBind(Intent? _Int)
+    {
+#if ANDROID29_0_OR_GREATER
+        return new Binder(_Int.Action);
+#endif
+        throw new Exception("idfk what this function's supposed to do");
+    }
+
+    public Device DType()
+    { return Device.Android; }
+
+    public bool CheckLocation()
+    {
+        using (LocationManager LM = (LocationManager)CTX.GetSystemService(LocationService))
+        {
+#if ANDROID28_0_OR_GREATER
+            return LM.IsLocationEnabled;
+#endif
+        }
+    }
+
+    #region Old
+    //is called to collect data
+    /*public IEnumerable<WifiInfoItem> GetData()
+    {
+        //SDD.WriteLine("**** Get Data called ****");
+
+        if (CM == null)
+        { throw new Exception("CM was null!"); }
+
+        if (IsRegistered)
+        {
+            CM.UnregisterNetworkCallback(NetCB);
+            IsRegistered = false;
+        }
+
+        if (!IsRegistered)
+        {
+            CM.RegisterNetworkCallback
+            (
+                new NetworkRequest.Builder().AddTransportType(TransportType.Wifi).Build(),
+                NetCB
+            );
+            IsRegistered = true;
+        }
+
+        //SDD.WriteLine($"***** CollectData results {ScanData.Count} *****");
+
+        return ScanData;
+    } */
+
+
+    #endregion
+}
+
+public class SRC : WifiManager.ScanResultsCallback
+{
+    public Action CallBack { get; set; }
+
+    public void Initialise(Action _CallBack)
+    { CallBack = _CallBack; }
+
+    public SRC()
+    { }
+
+    public override void OnScanResultsAvailable()
+    { CallBack(); }
 }
--- WifiItem.cs
+++ WifiItem.cs
@@ -10,231 +10,230 @@ using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
 
-namespace WifiScannerLib
+namespace WifiScannerLib;
+
+public interface IWS
 {
-    public interface IWS
-    {
-        public event EventHandler ScanReturned;
+    public event EventHandler ScanReturned;
 
-        public Device DType();
-        public bool CheckLocation();
-        public void CollectData();
-    }
+    public Device DType();
+    public bool CheckLocation();
+    public void CollectData();
+}
 
-    /// <summary>
-    /// Holds relevant data about APs
-    /// </summary>
-    public class WifiInfoItem : IEquatable<WifiInfoItem>
-    {
-        //Android-specific shenanigans
-        #region Android
+/// <summary>
+/// Holds relevant data about APs
+/// </summary>
+public class WifiInfoItem : IEquatable<WifiInfoItem>
+{
+    //Android-specific shenanigans
+    #region Android
 #if ANDROID
-        public WifiInfoItem(ScanResult _SR)
-        {
+    public WifiInfoItem(ScanResult _SR)
+    {
 #if ANDROID33_0_OR_GREATER
-            SSID = _SR.WifiSsid.ToString();
+        SSID = _SR.WifiSsid.ToString();
 #elif ANDROID21_0_OR_GREATER
-            SSID = _SR.Ssid;
+        SSID = _SR.Ssid;
 #endif
-            BSSID = _SR.Bssid;
+        BSSID = _SR.Bssid;
 
-            _RSSI = _SR.Level;
-            LastUpdated = TimeSpan.FromMicroseconds(_SR.Timestamp);
+        _RSSI = _SR.Level;
+        LastUpdated = TimeSpan.FromMicroseconds(_SR.Timestamp);
 
-            if (SSID == "")
+        if (SSID == "")
 	        {SSID = "*Hidden*";}
-            else
+        else
 	        {SSID = SSID.Trim('\"', ' ');}
 
 
-            PrimaryFrequency = _SR.Frequency;
+        PrimaryFrequency = _SR.Frequency;
 
-            _Distance = DistanceCalc(PrimaryFrequency, _RSSI);
-        }
+        _Distance = DistanceCalc(PrimaryFrequency, _RSSI);
+    }
 #endif
-        #endregion
+    #endregion
 
-        //IOS-specific shenanigans
-        #region IOS
+    //IOS-specific shenanigans
+    #region IOS
 #if IOS
-        public WifiInfoItem(NetworkExtension.NEHotspotHelperResult NEHHR)
-        {
-        }
+    public WifiInfoItem(NetworkExtension.NEHotspotHelperResult NEHHR)
+    {
+    }
 #endif
-        #endregion
+    #endregion
 
-        public WifiInfoItem()
-        { }
+    public WifiInfoItem()
+    { }
 
-        [JsonInclude]
-        public string BSSID { get; set; } = string.Empty;
+    [JsonInclude]
+    public string BSSID { get; set; } = string.Empty;
 
-        [JsonInclude]
-        public string SSID { get; set; } = string.Empty;
+    [JsonInclude]
+    public string SSID { get; set; } = string.Empty;
 
-        [JsonInclude]
-        public int _RSSI { get; private set; } = -101;
+    [JsonInclude]
+    public int _RSSI { get; private set; } = -101;
 
-        [JsonIgnore]
-        public string RSSI
-        {
-            get => _RSSI.ToString("#dbm");
-            set => _RSSI = int.Parse(value.Replace("dbm", ""));
-        }
-
-        [JsonIgnore]
-        public string Capabilities { get; set; } = string.Empty;
-
-        [JsonInclude]
-        public TimeSpan LastUpdated { get; set; } = TimeSpan.Zero;
-        //https://developer.android.com/reference/android/net/wifi/ScanResult#timestamp
-
-        [JsonInclude]
-        public double _Distance { get; private set; } = 0d;
-
-        [JsonIgnore]
-        public string Distance
-        {
-            get => $"{_Distance.ToString("F2")}m";
-            set => _Distance = double.Parse(value);
-        }
-
-        [JsonInclude]
-        public double PrimaryFrequency { get; set; } = 0d;
-
-        public static double DistanceCalc(double _Frequency, float _RSSI)
-        {
-            //10^((27.55 - 20*log10(f) + |R|)/20)
-            return Math.Pow(10, (27.55d - 20 * Math.Log10(_Frequency) + Math.Abs(_RSSI)) / 20d);
-        }
-
-        public WifiInfoItem Clone()
-        { return this.MemberwiseClone() as WifiInfoItem; }
-
-        public bool Equals(WifiInfoItem A, WifiInfoItem B)
-        {
-            if (A.BSSID == B.BSSID)
-            { return true; }
-            else
-            { return false; }
-        }
-
-        public bool Equals(WifiInfoItem B)
-        {
-            if (this.BSSID == B.BSSID)
-            { return true; }
-            else
-            { return false; }
-        }
-
-        public override string ToString()
-        { return $"BSSID: {BSSID}, SSID: {SSID}, RSSI: {RSSI}, Distance: {Distance}"; }
-
-        public SigLevel GetLevel()
-        {
-            if (_RSSI > -34) return SigLevel.L1;
-            else if (_RSSI > -39) return SigLevel.L2;
-            else if (_RSSI > -44) return SigLevel.L3;
-            else if (_RSSI > -49) return SigLevel.L4;
-            else if (_RSSI > -54) return SigLevel.L5;
-            else if (_RSSI > -64) return SigLevel.L6;
-            else if (_RSSI > -74) return SigLevel.L7;
-            else if (_RSSI > -84) return SigLevel.L8;
-            else return SigLevel.L9;
-        }
-
-        public static SigLevel GetLevel(int _RSSI)
-        {
-            if (_RSSI > -34) return SigLevel.L1;
-            else if (_RSSI > -39) return SigLevel.L2;
-            else if (_RSSI > -44) return SigLevel.L3;
-            else if (_RSSI > -49) return SigLevel.L4;
-            else if (_RSSI > -54) return SigLevel.L5;
-            else if (_RSSI > -64) return SigLevel.L6;
-            else if (_RSSI > -74) return SigLevel.L7;
-            else if (_RSSI > -84) return SigLevel.L8;
-            else return SigLevel.L9;
-        }
-    }
-
-    public class WifiEvent : EventArgs
+    [JsonIgnore]
+    public string RSSI
     {
-        public Dictionary<string, WifiInfoItem> Data { get; private set; }
-        public int Count { get; private set; } = 0;
-
-        public WifiEvent()
-        {
-            Data = new Dictionary<string, WifiInfoItem>();
-            Count = 0;
-        }
-
-        public WifiEvent(IEnumerable<WifiInfoItem> _Wifis)
-        {
-            Count = _Wifis.Count();
-
-            //you fucking idiot
-            Data = _Wifis.ToDictionary(X => X.BSSID, Y => Y);
-            //^ this was missing
-        }
-
-        public WifiEvent(Dictionary<string, WifiInfoItem> _Data)
-        {
-            Data = _Data;
-            Count = Data.Count;
-        }
+        get => _RSSI.ToString("#dbm");
+        set => _RSSI = int.Parse(value.Replace("dbm", ""));
     }
 
-    public record SnapshotData
+    [JsonIgnore]
+    public string Capabilities { get; set; } = string.Empty;
+
+    [JsonInclude]
+    public TimeSpan LastUpdated { get; set; } = TimeSpan.Zero;
+    //https://developer.android.com/reference/android/net/wifi/ScanResult#timestamp
+
+    [JsonInclude]
+    public float _Distance { get; private set; } = 0f;
+
+    [JsonIgnore]
+    public string Distance
     {
-        [JsonInclude]
-        public int Index { get; set; } = 0;
-        [JsonInclude]
-        public TimeSpan LastUpdated { get; set; }
-        [JsonInclude]
-        public Dictionary<string, WifiInfoItem> Data { get; set; }
-
-        public SnapshotData()
-        {
-            LastUpdated = default;
-            Data = default;
-            Index = 0;
-        }
-
-        public SnapshotData(TimeSpan _LastUp, Dictionary<string, WifiInfoItem> _Data, int _Index)
-        {
-            LastUpdated = _LastUp;
-            Data = _Data;
-            Index = _Index;
-        }
-
-        public SnapshotData(JsonNode _JNode)
-        {
-            LastUpdated = _JNode["LastUpdated"].Deserialize<TimeSpan>();
-            Data = _JNode["Data"].Deserialize<Dictionary<string, WifiInfoItem>>();
-            Index = _JNode["Index"].Deserialize<int>();
-        }
+        get => $"{_Distance.ToString("F2")}m";
+        set => _Distance = float.Parse(value);
     }
 
-    public enum Device
+    [JsonInclude]
+    public float PrimaryFrequency { get; set; } = 0f;
+
+    public static float DistanceCalc(float _Frequency, float _RSSI)
     {
-        Android,
-        IOS,
-        Mac,
-        Linux,
-        Windows
+        //10^((27.55 - 20*log10(f) + |R|)/20)
+        return (float)Math.Pow(10, (27.55d - 20 * Math.Log10(_Frequency) + Math.Abs(_RSSI)) / 20d);
     }
 
-    [Flags]
-    public enum SigLevel
+    public WifiInfoItem Clone()
+    { return this.MemberwiseClone() as WifiInfoItem; }
+
+    public bool Equals(WifiInfoItem A, WifiInfoItem B)
     {
-        L1 = 1,
-        L2 = 2,
-        L3 = 4,
-        L4 = 8,
-        L5 = 16,
-        L6 = 32,
-        L7 = 64,
-        L8 = 128,
-        L9 = 256,
+        if (A.BSSID == B.BSSID)
+        { return true; }
+        else
+        { return false; }
     }
+
+    public bool Equals(WifiInfoItem B)
+    {
+        if (this.BSSID == B.BSSID)
+        { return true; }
+        else
+        { return false; }
+    }
+
+    public override string ToString()
+    { return $"BSSID: {BSSID}, SSID: {SSID}, RSSI: {RSSI}, Distance: {Distance}"; }
+
+    public SigLevel GetLevel()
+    {
+        if (_RSSI > -34) return SigLevel.L1;
+        else if (_RSSI > -39) return SigLevel.L2;
+        else if (_RSSI > -44) return SigLevel.L3;
+        else if (_RSSI > -49) return SigLevel.L4;
+        else if (_RSSI > -54) return SigLevel.L5;
+        else if (_RSSI > -64) return SigLevel.L6;
+        else if (_RSSI > -74) return SigLevel.L7;
+        else if (_RSSI > -84) return SigLevel.L8;
+        else return SigLevel.L9;
+    }
+
+    public static SigLevel GetLevel(int _RSSI)
+    {
+        if (_RSSI > -34) return SigLevel.L1;
+        else if (_RSSI > -39) return SigLevel.L2;
+        else if (_RSSI > -44) return SigLevel.L3;
+        else if (_RSSI > -49) return SigLevel.L4;
+        else if (_RSSI > -54) return SigLevel.L5;
+        else if (_RSSI > -64) return SigLevel.L6;
+        else if (_RSSI > -74) return SigLevel.L7;
+        else if (_RSSI > -84) return SigLevel.L8;
+        else return SigLevel.L9;
+    }
+}
+
+public class WifiEvent : EventArgs
+{
+    public Dictionary<string, WifiInfoItem> Data { get; private set; }
+    public int Count { get; private set; } = 0;
+
+    public WifiEvent()
+    {
+        Data = new Dictionary<string, WifiInfoItem>();
+        Count = 0;
+    }
+
+    public WifiEvent(IEnumerable<WifiInfoItem> _Wifis)
+    {
+        Count = _Wifis.Count();
+
+        //you fucking idiot
+        Data = _Wifis.ToDictionary(X => X.BSSID, Y => Y);
+        //^ this was missing
+    }
+
+    public WifiEvent(Dictionary<string, WifiInfoItem> _Data)
+    {
+        Data = _Data;
+        Count = Data.Count;
+    }
+}
+
+public record SnapshotData
+{
+    [JsonInclude]
+    public int Index { get; set; } = 0;
+    [JsonInclude]
+    public TimeSpan LastUpdated { get; set; }
+    [JsonInclude]
+    public Dictionary<string, WifiInfoItem> Data { get; set; }
+
+    public SnapshotData()
+    {
+        LastUpdated = default;
+        Data = default;
+        Index = 0;
+    }
+
+    public SnapshotData(TimeSpan _LastUp, Dictionary<string, WifiInfoItem> _Data, int _Index)
+    {
+        LastUpdated = _LastUp;
+        Data = _Data;
+        Index = _Index;
+    }
+
+    public SnapshotData(JsonNode _JNode)
+    {
+        LastUpdated = _JNode["LastUpdated"].Deserialize<TimeSpan>();
+        Data = _JNode["Data"].Deserialize<Dictionary<string, WifiInfoItem>>();
+        Index = _JNode["Index"].Deserialize<int>();
+    }
+}
+
+public enum Device
+{
+    Android,
+    IOS,
+    Mac,
+    Linux,
+    Windows
+}
+
+[Flags]
+public enum SigLevel
+{
+    L1 = 1,
+    L2 = 2,
+    L3 = 4,
+    L4 = 8,
+    L5 = 16,
+    L6 = 32,
+    L7 = 64,
+    L8 = 128,
+    L9 = 256,
 }
